0bd0967fa159f17eaed1523c853bd3dd9713b5fd
Alex O'Regan <alexsoregan@gmail.com>
2022-02-04 21:22:53 +0000

    bashrc -> bash_profile

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-cp ~/.bashrc ./bashrc.backup
-cp ./bashrc ~/.bashrc
+cp ~/.bash_profile ./bash_profile.backup
+cp ./bash_profile ~/.bash_profile
 
 cp ~/.bash_aliases ./bash_aliases.backup
 cp ./bash_aliases ~/.bash_aliases
