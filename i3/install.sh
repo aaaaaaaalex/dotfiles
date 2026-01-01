364ce824037281510d39b3c511128730e15a63ec
Alex O'Regan <alexsoregan@gmail.com>
2026-01-01 13:40:42 +0000

    ditch i3

--- i3/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cp ~/.config/i3/config ./config.old
-cp ./config ~/.config/i3/config
-
-chmod 755 ./scratchpad
-sudo cp ./scratchpad /usr/local/bin/scratchpad
-
-i3-msg restart
--- i3status/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp /etc/i3status.conf ./i3status.conf.old
-cp ./i3status.conf /etc/i3status.conf
